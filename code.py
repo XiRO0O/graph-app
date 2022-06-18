2a5a01bcf492f81c804afba25357816839ce2b56
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 11:30:10 +0200

    sumbit number

--- code.py
+++ code.py
@@ -1,13 +1,15 @@
 import PySimpleGUI as sg
 
+sg.theme('Dark')
 table_content = []
 layout = [
     [sg.Table(
-    headings = ['Observation','Result'],
-    values = table_content,
-    expand_x = True,
-    hide_vertical_scroll = True,
-    key = '-TABLE-')],
+        headings = ['Observation','Result'],
+        values = table_content,
+        expand_x = True,
+        hide_vertical_scroll = True,
+        key = '-TABLE-')],
+    [sg.Input(key = '-INPUT-', expand_x = True), sg.Button('Sumbit')]
 ]
 
 window = sg.Window('Graph App', layout)
@@ -16,5 +18,11 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event == 'Sumbit':
+        new_value = values['-INPUT-']
+        if new_value.isnumeric():
+            table_content.append([len(table_content) + 1,float(new_value)])
+            window ['-TABLE-'].update(table_content)
 
 window.close()
